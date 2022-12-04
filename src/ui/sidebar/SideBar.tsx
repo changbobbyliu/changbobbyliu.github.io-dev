d77f11d4163acc739d7edfbcf6db41989ff5981c
Chang Liu <changbobbyliu@gmail.com>
2022-12-04 15:43:49 -0800

    dark mode switch

--- src/ui/sidebar/SideBar.tsx
+++ src/ui/sidebar/SideBar.tsx
@@ -1,8 +1,7 @@
-import { useDarkMode } from "@/hooks/useDarkMode";
+import { DarkModeSwitch } from "../dark-mode-switch/DarkModeSwitch";
 import styles from "./SideBar.module.css";
 
 export function SideBar() {
-	const [darkMode, setDarkMode] = useDarkMode();
 	return (
 		<div className="bg-gray-100 dark:bg-primary text-white fixed top-0 left-0 h-screen w-16 flex flex-col items-center py-4 space-y-4 shadow-lg transition-colors">
 			<SideBarItem icon="icTreeRound" tooltip="💡 LANDING" />
@@ -11,17 +10,7 @@ export function SideBar() {
 			<SideBarItem icon="icDatabase" tooltip="💡 SERVER" />
 			<SideBarItem icon="icComputerTablet" tooltip="💡 MOBILE" />
 			<div className="flex flex-1 flex-col justify-end">
-				<div
-					className="text-2xl rounded-full bg-primary-100 w-12 h-12 flex justify-center items-center hover:brightness-105 cursor-pointer"
-					onClick={() => {
-						if (typeof setDarkMode === "boolean") {
-						} else {
-							setDarkMode(!darkMode);
-						}
-					}}
-				>
-					<span>{darkMode ? "🌛" : "🌞"}</span>
-				</div>
+				<DarkModeSwitch />
 			</div>
 		</div>
 	);
