7bb8203a5d1babf4f3c338261cc635b634f0434e
Chang Liu <changbobbyliu@gmail.com>
2022-12-04 18:11:42 -0800

    mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { SideBar } from "@/ui/sidebar/SideBar";
 import { GProvider } from "@/managers/context/GContext";
+import { MainContent } from "@/ui/main-content/MainContent";
 
 function App() {
 	return (
 		<GProvider>
 			<div>
 				<SideBar />
-				<div className="ml-16 flex justify-center items-center min-h-screen bg-white transition-colors dark:bg-primary-100 dark:text-white">
-					<h1 className="text-lg uppercase font-mono">👋 Hello, Friend!</h1>
-				</div>
+				<MainContent />
 			</div>
 		</GProvider>
 	);
--- src/config/mockdata/index.ts
+++ src/config/mockdata/index.ts
@@ -1,4 +1,4 @@
-type TSideBarCategory = typeof sideBarCategories[number];
+export type TSideBarCategory = typeof sideBarCategories[number];
 
 type TSideBarItem = {
 	category: TSideBarCategory;
@@ -35,7 +35,68 @@ const sideBarItems: { [key: string]: TSideBarItem } = {
 	},
 };
 
+const socials: { icon: NSChangGlobal.TIconName; tintClassName?: string; url: string }[] = [
+	{ icon: "icGithub", url: "https://github.com/changbobbyliu" },
+	{ icon: "icTwitter", tintClassName: "text-sky-400", url: "https://twitter.com/changisadev" },
+	{
+		icon: "icLinkedin",
+		tintClassName: "text-blue-600",
+		url: "https://www.linkedin.com/in/bobbychangliu",
+	},
+];
+
+const techstacks = [
+	"React",
+	"React Native",
+	"Java",
+	"Objective-c",
+	"Nextjs",
+	"Solidity",
+	"Cocos Creator",
+] as const;
+type TTechstack = typeof techstacks[number];
+
+export type TPortfolioItem = {
+	title: string;
+	description: string;
+	url: string;
+	categories: TSideBarCategory[];
+	techstacks: TTechstack[];
+};
+const portfolios: TPortfolioItem[] = [
+	{
+		title: "A Web3 GameFi Dapp",
+		description: "A Web3 GameFi Dapp",
+		url: "https://changliu.is-a.dev/bs-game-front",
+		categories: ["web3", "game"],
+		techstacks: ["React", "Solidity"],
+	},
+	{
+		title: "A Web3 ENS Dapp",
+		description: "A Web3 ENS Dapp",
+		url: "https://changliu.is-a.dev/bs-ens-front",
+		categories: ["web3"],
+		techstacks: ["React", "Solidity"],
+	},
+	{
+		title: "A Web3 DAO Dapp",
+		description: "A Web3 DAO Dapp",
+		url: "https://changliu.is-a.dev/bs-dao",
+		categories: ["web3"],
+		techstacks: ["React", "Solidity"],
+	},
+	{
+		title: "A Cocos Game",
+		description: "A Cocos Game",
+		url: "https://chang.is-a.dev/dh_hack2022",
+		categories: ["game"],
+		techstacks: ["Cocos Creator"],
+	},
+];
+
 export const mockData = {
 	sideBarCategories,
 	sideBarItems,
+	socials,
+	portfolios,
 };
