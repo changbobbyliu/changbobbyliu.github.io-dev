7ec22b5caab4fc1d0f395145f129a1996c9dd47e
Chang Liu <changbobbyliu@gmail.com>
2023-01-14 12:07:51 -0800

    esbuild transform

--- src/ui/screens/landing/sections/TestEsbuild.section.tsx
+++ src/ui/screens/landing/sections/TestEsbuild.section.tsx
@@ -1,5 +1,5 @@
 import { H1 } from "@/ui/components";
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import * as esbuild from "esbuild-wasm";
 
 let isEsbuildReady = false;
@@ -29,8 +29,13 @@ const useInitEsbuild = (onError?: (error: Error) => void) => {
 	return isReady;
 };
 
+/**
+ * const App = () => <div>APP</div>;
+ */
 export const TestEsbuildSection = memo<{ containerClassName?: string }>(
 	({ containerClassName = "" }) => {
+		const inputRef = useRef<HTMLTextAreaElement>(null);
+		const [code, setCode] = useState("");
 		const isReady = useInitEsbuild((error) => {
 			console.log("useInitEsbuild.error", error);
 		});
@@ -38,10 +43,25 @@ export const TestEsbuildSection = memo<{ containerClassName?: string }>(
 			<div className={`flex flex-col items-center w-full ${containerClassName}`}>
 				<H1 containerClassName="mb-8">Esbuild IDE</H1>
 				<div className="w-full flex flex-col">
-					<textarea className="w-full bg-slate-900 rounded-md p-2 mb-2"></textarea>
-					<button disabled={!isReady} className="rounded-lg bg-accent p-2 self-end">
+					<textarea ref={inputRef} className="w-full bg-slate-900 rounded-md p-2 mb-2"></textarea>
+					<button
+						disabled={!isReady}
+						className="rounded-lg bg-accent p-2 self-end disabled:bg-gray-400 disabled:text-gray-300 mb-2"
+						onClick={async () => {
+							const inputText = inputRef.current?.value;
+							esbuild
+								.transform(inputText || "", {
+									loader: "jsx",
+									target: "es2015",
+								})
+								.then((result) => {
+									setCode(result.code);
+								});
+						}}
+					>
 						Compile
 					</button>
+					<p className="w-full bg-slate-800 font-mono rounded-md p-2">{code}</p>
 				</div>
 			</div>
 		);
