b2afb37eb5bc2e1dc30ac54741075ed01e413b2e
Chang Liu <changbobbyliu@gmail.com>
2023-01-14 11:32:58 -0800

    init esbuild

--- src/ui/screens/landing/sections/TestEsbuild.section.tsx
+++ src/ui/screens/landing/sections/TestEsbuild.section.tsx
@@ -1,12 +1,55 @@
 import { H1 } from "@/ui/components";
-import { FC } from "react";
+import { memo, useEffect, useState } from "react";
+import * as esbuild from "esbuild-wasm";
 
-export const TestEsbuildSection: FC<{ containerClassName?: string }> = ({
-	containerClassName = "",
-}) => {
-	return (
-		<div className={`${containerClassName}`}>
-			<H1>Esbuild IDE</H1>
-		</div>
-	);
+let isEsbuildReady = false;
+
+const startService = async (onError?: (error: Error) => void) => {
+	if (isEsbuildReady) return true;
+	console.log("isEsbuildReady", isEsbuildReady);
+	await esbuild
+		.initialize({
+			wasmURL: "/esbuild.wasm",
+			worker: true,
+		})
+		.then(() => {
+			isEsbuildReady = true;
+		})
+		.catch((error) => {
+			onError?.(error);
+		});
+	return isEsbuildReady;
 };
+
+const useInitEsbuild = (onError?: (error: Error) => void) => {
+	const [isReady, setIsReady] = useState(false);
+	useEffect(() => {
+		startService(onError).then(setIsReady);
+	}, [onError]);
+	return isReady;
+};
+
+export const TestEsbuildSection = memo<{ containerClassName?: string }>(
+	({ containerClassName = "" }) => {
+		const isReady = useInitEsbuild((error) => {
+			console.log("useInitEsbuild.error", error);
+		});
+		return (
+			<div className={`flex flex-col items-center w-full ${containerClassName}`}>
+				<H1 containerClassName="mb-8">Esbuild IDE</H1>
+				<div className="w-full flex flex-col">
+					<textarea className="w-full bg-slate-900 rounded-md p-2 mb-2"></textarea>
+					<button disabled={!isReady} className="rounded-lg bg-accent p-2 self-end">
+						Compile
+					</button>
+				</div>
+			</div>
+		);
+	}
+);
+
+// export const TestEsbuildSection: FC<{ containerClassName?: string }> = ({
+// 	containerClassName = "",
+// }) => {
+
+// };
