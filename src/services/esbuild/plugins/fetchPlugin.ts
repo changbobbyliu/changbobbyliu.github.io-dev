a4c2ed737d2f44d9dcb48a5fc884ee672e0a839e
Chang Liu <changbobbyliu@gmail.com>
2023-01-15 22:49:40 -0800

    load css

--- src/services/esbuild/plugins/fetchPlugin.ts
+++ src/services/esbuild/plugins/fetchPlugin.ts
@@ -4,25 +4,47 @@ import { C } from "@/config/constants";
 export const fetchPlugin = (inputCode: string): esbuild.Plugin => ({
 	name: "fetch-plugin",
 	setup(build: esbuild.PluginBuild) {
-		// 8. overide the default onLoad to return the contents of the file
-		// 9. if import -> run onResolve again
-		build.onLoad({ filter: /.*/ }, async (args) => {
-			if (args.path === "index.js")
-				return {
-					loader: "jsx",
-					// 10. Possible to specify version, like react@16.0.0
-					contents: inputCode,
-				};
+		// 1. onLoad added order matters!
+		build.onLoad({ filter: /^index\.js$/ }, () => ({
+			loader: "jsx",
+			// 10. Possible to specify version, like react@16.0.0
+			contents: inputCode,
+		}));
 
+		build.onLoad({ filter: /.*/ }, async (args) => {
 			// 11. Check if file is in cache
 			const cachedResult = await C.fileCache.getItem<OnLoadResult>(args.path);
 			if (cachedResult) return cachedResult;
+			// 11. move to next onLoad till return is not undefined/null/etc
+		});
 
-			// 12. Fetch the file from unpkg
+		// handle css
+		build.onLoad({ filter: /\.css$/ }, async (args) => {
 			const resData = await fetch(args.path);
+			const contents = await resData
+				.text()
+				.then((rawText) => rawText.replace(/\n/g, "").replace(/"/g, '\\"').replace(/'/g, "\\'"))
+				.then(
+					(escaped) =>
+						`const style = document.createElement('style');style.innerText = '${escaped}';document.head.appendChild(style);`
+				);
 			const result: OnLoadResult = {
 				loader: "jsx",
-				contents: await resData.text(),
+				contents,
+				resolveDir: new URL("./", resData.url).pathname,
+			};
+			await C.fileCache.setItem(args.path, result);
+			return result;
+		});
+
+		// 9. if import -> run onResolve again
+		build.onLoad({ filter: /.*/ }, async (args) => {
+			// 12. Fetch the file from unpkg
+			const resData = await fetch(args.path);
+			const contents = await resData.text();
+			const result: OnLoadResult = {
+				loader: "jsx",
+				contents,
 				resolveDir: new URL("./", resData.url).pathname,
 			};
 			await C.fileCache.setItem(args.path, result);
